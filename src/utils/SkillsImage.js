617c8d149782d0f452af4a5d8ad64b4c17a46867
kkhushiie <k.khushisharma18@gmail.com>
2023-07-26 12:59:37 +0530

    second commit

--- src/utils/SkillsImage.js
+++ src/utils/SkillsImage.js
@@ -1,28 +1,21 @@
-import html from '../assets/svg/skills/html.svg'
+import html from '../Assets/svg/skills/html.svg'
 
-import css from '../assets/svg/skills/css.svg'
-import javascript from '../assets/svg/skills/javascript.svg'
-import nextJS from '../assets/svg/skills/nextJS.svg'
-import react from '../assets/svg/skills/react.svg'
-import typescript from '../assets/svg/skills/typescript.svg'
-import bootstrap from '../assets/svg/skills/bootstrap.svg'
-
-import mysql from '../assets/svg/skills/mysql.svg'
-import c from '../assets/svg/skills/c.svg'
-
-import java from '../assets/svg/skills/java.svg'
-
-import matlab from '../assets/svg/skills/matlab.svg'
-import python from '../assets/svg/skills/python.svg'
-
-import git from '../assets/svg/skills/git.svg'
-
-import materialui from '../assets/svg/skills/materialui.svg'
-import figma from '../assets/svg/skills/figma.svg'
-
-import microsoftoffice from '../assets/svg/skills/microsoftoffice.svg'
-import canva from '../assets/svg/skills/canva.svg'
-
+import css from '../Assets/svg/skills/css.svg'
+import javascript from '../Assets/svg/skills/javascript.svg'
+import nextJS from '../Assets/svg/skills/nextJS.svg'
+import react from '../Assets/svg/skills/react.svg'
+import typescript from '../Assets/svg/skills/typescript.svg'
+import bootstrap from '../Assets/svg/skills/bootstrap.svg'
+import mysql from '../Assets/svg/skills/mysql.svg'
+import c from '../Assets/svg/skills/c.svg'
+import java from '../Assets/svg/skills/java.svg'
+import matlab from '../Assets/svg/skills/matlab.svg'
+import python from '../Assets/svg/skills/python.svg'
+import git from '../Assets/svg/skills/git.svg'
+import materialui from '../Assets/svg/skills/materialui.svg'
+import figma from '../Assets/svg/skills/figma.svg'
+import microsoftoffice from '../Assets/svg/skills/microsoftoffice.svg'
+import canva from '../Assets/svg/skills/canva.svg'
 
 export const skillsImage = (skill) => {
     const skillID = skill.toLowerCase();
