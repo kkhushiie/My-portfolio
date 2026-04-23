01ca83100d1091d56e94fc44e73ebf903a4e75f5
Khushi Sharma <k.khushisharma18@gmail.com>
2026-04-23 21:39:33 +0530

    fix: node 24

---
+++ OneDrive/Desktop/My-portfolio/src/components/Text.jsx
@@ -0,0 +1,24 @@
+import Typewriter from "typewriter-effect";
+import "../pages/style.css";
+
+
+function Text() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "React Js Developer",
+          "Computer Science Undergraduate",
+          "Front End Developer",
+          //"Front End Developer",
+
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 30,
+      }}
+    />
+  )
+}
+
+export default Text
